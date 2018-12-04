31dd0d318005ed24ad33d97d3440d8d5c624784c
Kellen1 <45210664+Kellen1@users.noreply.github.com>
2018-12-04 14:10:06 +0200

    added my work

---
+++ Pro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kellen
+{
+	class Pro
+	{
+		static void Main(string[] args)
+		{
+			int r;
+			int t;
+			string a;
+			Console.WriteLine("enter two numbers");
+			r = Convert.ToInt32(Console.ReadLine());
+			t = Convert.ToInt32(Console.ReadLine());
+
+			var olly = new calculator();
+
+			Console.WriteLine("enter an operator");
+			a = Console.ReadLine();
+			if(a=="+")
+			Console.WriteLine("addition: {0}",olly.add(r,t));
+
+			if (a == "-")
+				Console.WriteLine("subtraction: {0}", olly.subtruction(r, t));
+
+			if (a == "*")
+				Console.WriteLine("multiplication: {0}", olly.multiplication(r, t));
+
+			if (a == "/")
+				Console.WriteLine("division: {0}", olly.division(r, t));
+
+			else
+				Console.WriteLine("invalid");
+
+
+
+			Console.ReadLine();
+
+		}
+	}
+}
