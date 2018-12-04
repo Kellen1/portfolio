dc839f6b36baf09a461ee65aea191dc784d9ae74
Kellen1 <45210664+Kellen1@users.noreply.github.com>
2018-12-04 14:01:57 +0200

    added a class and its source code

---
+++ calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kellen
+{
+	class calculator
+	{
+		public int n1;
+		public int n2;
+
+	public int add(int n1, int n2)
+		{
+			return (n1 + n2);
+		}
+		public int subtruction(int n1, int n2)
+		{
+			return (n1 - n2);
+		}
+		public int multiplication(int n1, int n2)
+		{
+			return (n1 * n2);
+		}
+		public int division(int n1, int n2)
+		{
+			return (n1 / n2);
+		}
+
+
+	}
+}
